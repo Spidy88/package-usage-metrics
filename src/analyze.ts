3fa4f94b7f1f28026189aefabd01eb0a0718b095
Nick Ferraro <the.nick.ferraro@gmail.com>
2023-09-18 20:18:08 -0700

    WIP: Usage analyzer

--- src/analyze.ts
+++ src/analyze.ts
@@ -1,12 +1,15 @@
 import child_process from 'child_process';
 import fs from 'fs';
+import { globSync } from 'glob';
+import babelParser from '@babel/parser';
 
 function read() {
     
 }
 
+// https://github.com/indatawetrust/package-usage-analyzer/blob/main/lib/index.js
 function analyzeUsage() {
-    
+    const files = globSync('src/**/*.{j,t}{s,sx}', { ignore: 'node_modules' });
 }
 
 type Commit = {
@@ -42,9 +45,13 @@ function checkoutHash(hash: string) {
     child_process.execSync(`git checkout ${hash}`);
 }
 
-const commits = getCommits();
-for (const commit of commits) {
-    console.log(`(${commit.hash.substring(0, 5)}): On ${commit.date}, ${commit.author} committed ${commit.message}`);
-    checkoutHash(commit.hash);
-    const report = analyzeUsage();
+function run() {
+    const commits = getCommits();
+    for (const commit of commits) {
+        console.log(`(${commit.hash.substring(0, 5)}): On ${commit.date}, ${commit.author} committed ${commit.message}`);
+        checkoutHash(commit.hash);
+        const report = analyzeUsage();
+    }
 }
+
+run();
