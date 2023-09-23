fc5977e0dc53f4c2606caeb811998a832726ae49
Nick Ferraro <the.nick.ferraro@gmail.com>
2023-09-22 21:12:54 -0700

    Save

--- src/analyze.ts
+++ src/analyze.ts
@@ -1,7 +1,9 @@
 import child_process from 'child_process';
 import fs from 'fs';
+import path from 'path';
 import { globSync } from 'glob';
 import babelParser from '@babel/parser';
+import { ImportDeclaration, is, isType, isImportDeclaration, Statement, VariableDeclaration, isVariableDeclaration, CallExpression } from '@babel/types';
 
 function read() {
     
@@ -9,7 +11,39 @@ function read() {
 
 // https://github.com/indatawetrust/package-usage-analyzer/blob/main/lib/index.js
 function analyzeUsage() {
-    const files = globSync('src/**/*.{j,t}{s,sx}', { ignore: 'node_modules' });
+    const allPackages = new Map<string, number>();
+    const files = globSync('src/**/*.{j,t}{s,sx}', { ignore: '**/node_modules/**' });
+    for (const file of files) {
+        const contents = fs.readFileSync(file, 'utf-8');
+        const ast = babelParser.parse(contents, {
+            errorRecovery: true,
+            sourceType: "unambiguous",
+            plugins: [
+              "jsx"
+            ]
+        });
+
+        const importDeclarations = ast.program.body.filter<ImportDeclaration>((node: Statement): node is ImportDeclaration => isImportDeclaration(node));
+        const varDeclarations = ast.program.body.filter<VariableDeclaration>((node: Statement): node is VariableDeclaration => isVariableDeclaration(node));
+        varDeclarations[0].declarations[0].type === 'VariableDeclarator'
+varDeclarations[0].declarations[0].id.type === 'Identifier' || 'ObjectPattern'
+varDeclarations[0].declarations[0].init?.type === 'CallExpression'
+const callExpression = varDeclarations[0].declarations[0].init as unknown as CallExpression;
+callExpression.callee.type === 'Identifier'
+callExpression.callee?.name === 'require';
+callExpression.arguments[0].type === 'StringLiteral';
+callExpression.arguments[0].value == 'fs';
+
+        for (let i of importDeclarations) {
+            let packageName = i.source.value
+            const usagePackages = i.specifiers.map(i => i.imported ? i.imported.name : i.local.name)
+            const usageCount = usagePackages.length;
+
+            if (packageName.match(/^[\.\\]/)) {
+                packageName = path.relative(currentDir, path.join(path.dirname(file), packageName))
+            }
+        }
+    }
 }
 
 type Commit = {
